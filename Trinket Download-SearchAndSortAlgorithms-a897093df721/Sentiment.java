426055810186b70d2fd7d6102066a728edebbc90
alfiya <147679431+albs31@users.noreply.github.com>
2026-04-23 01:11:25 -0700

    Update Sentiment.java

--- Trinket Download-SearchAndSortAlgorithms-a897093df721/Sentiment.java
+++ Trinket Download-SearchAndSortAlgorithms-a897093df721/Sentiment.java
@@ -1,12 +1,3 @@
-Anne of Green Gables, L.M. Montgomery, 4.25
-The Secret Garden, Frances Hodgson Burnett, 4.13
-A Little Princess, Frances Hodgson Burnett, 4.20
-The Wonderful Wizard of Oz, L. Frank Baum, 3.98
-Peter Pan, J.M. Barrie, 4.08
-The Railway Children, E. Nesbit, 4.00
-The Tale of Peter Rabbit, Beatrix Potter, 4.17
-Pollyanna, Eleanor H. Porter, 3.97
-
 /*
  * Project 4.1.5
  *
